09d4798d44760160f65fdecfbcf20e8e577eb373
Hexalopata <alekseygasyuk50@gmail.com>
2022-02-17 12:08:12 +0300

    Добавлен стартовый проект для фронтенда

---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, '../static/'),
+        filename: "[hash].[name].js"
+    },
+    resolve: { extensions: [".js", ".jsx"] },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            publicPath: '/static/'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [ "babel-loader" ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { modules: true }
+                    }
+                ]
+            }
+        ]
+    }
+};
--- notes_store/settings.py
+++ notes_store/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
